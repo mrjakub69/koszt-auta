43c5f870773c93f55945662cb429fecc4efebf5f
JK <[ziomek003xd@gmail.com](mailto:twojemail@gmail.com)>
2026-05-03 12:45:13 +0200

    start

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-bold">
+        Ile naprawdę kosztuje auto?
+      </h1>
+
+      <p className="mt-4 text-zinc-400">
+        Sprawdź realny koszt utrzymania samochodu.
+      </p>
+    </main>
+  );
+}
